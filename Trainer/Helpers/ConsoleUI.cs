d8aebf678607277453b6610212ab7a158c03ddb3
katja tsareva <katja.tcareva@gmail.com>
2025-06-02 14:37:22 +0200

    move console interactions to separate class

---
+++ Trainer/Helpers/ConsoleUI.cs
@@ -0,0 +1,32 @@
+namespace Trainer.Helpers;
+
+internal static class ConsoleUI
+{
+    public static int GetNumberInRange(string prompt, int min, int max = int.MaxValue)
+    {
+        int num;
+        bool isValid = false;
+        do
+        {
+            Output(prompt);
+            var input = Input();
+
+            if (int.TryParse(input, out num))
+            {
+                if (min < num && num <= max)
+                    isValid = true;
+            }
+        } while (!isValid);
+        return num;
+    }
+
+    public static string Input()
+    {
+        return Console.ReadLine()?.Trim() ?? "";
+    }
+
+    public static void Output(string output)
+    {
+        Console.WriteLine(output);
+    }
+}
